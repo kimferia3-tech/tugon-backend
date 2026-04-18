61e663ee79822b4f00fb84d4d522d29e95921028
kimferia3-tech <kimferia3@gmail.com>
2026-04-18 23:07:33 +0800

    Update server.js

--- server.js
+++ server.js
@@ -218,7 +218,6 @@ app.get('/notifications', async (req, res) => {
         res.status(200).json(result.rows);
     } catch (err) { res.status(500).json({ error: "Error" }); }
 });
-
 // --- 8. START SERVER ---
 const PORT = process.env.PORT || 3000; 
 server.listen(PORT, () => { console.log(`Server running on port ${PORT}`); });
